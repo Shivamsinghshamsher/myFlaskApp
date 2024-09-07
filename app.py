15c50612e41ec968115b6ad500aa2d96fc1dddda
Shivam_Singh_Shamsher <111301230+Shivamsinghshamsher@users.noreply.github.com>
2024-09-07 18:48:18 +0000

    Applied dynamic data and added api route

--- app.py
+++ app.py
@@ -1,9 +1,80 @@
-from flask import Flask 
+from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 4,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 5,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 5,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 5,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 5,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 5,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 5,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+]
+
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+   return render_template("index.html", jobs=JOBS)
+
+
+@app.route('/api/jobs')
+def list_jobs():
+   return jsonify(JOBS)
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
-  
+   app.run(host='0.0.0.0', port=3030, debug=True)
